68fb4b4d3aa6fa949097eeeffd4a7567546defa2
Mikhail Gavrilyak <gavrilyak.mikhail@gmail.com>
2025-04-27 14:58:44 +0500

    fix docker start

--- AccountService/AccountService/Data/DataSeeder.cs
+++ AccountService/AccountService/Data/DataSeeder.cs
@@ -1,6 +1,5 @@
 using AccountService.Models;
 using Microsoft.AspNetCore.Identity;
-using Microsoft.EntityFrameworkCore;
 
 namespace AccountService.Data;
 
@@ -18,14 +17,17 @@ public class DataSeeder
         using var scope = serviceProvider.CreateScope();
         var dbContext = scope.ServiceProvider.GetRequiredService<AccountDbContext>();
         var userManager = scope.ServiceProvider.GetRequiredService<UserManager<User>>();
-        
-        if (dbContext.Users.Any())
+     
+        Console.WriteLine("Start seeding...");
+
+        if (!await dbContext.Database.EnsureCreatedAsync())
         {
             return;
         }
 
-        await dbContext.Database.MigrateAsync();
         await SeedUsersAsync(userManager, dbContext);
+        await dbContext.SaveChangesAsync();
+        Console.WriteLine("Seeding ended...");
     }
 
     private static async Task SeedUsersAsync(UserManager<User> userManager, AccountDbContext dbContext)
@@ -33,6 +35,5 @@ public class DataSeeder
         var testUser = new User("temp@mail.ru", "Имя", "Фамилия", "Отчество");
 
         var result = await userManager.CreateAsync(testUser, "password");
-        await dbContext.SaveChangesAsync();
     }
 }
