4867e4e55ab42ea55fec034ce76b6a94e9ad4877
Mikhail Gavrilyak <gavrilyak.mikhail@gmail.com>
2025-05-24 18:35:18 +0500

    chore: api docs /user-scores

--- PIQService/PIQService.Api/Controllers/UserScoresController.cs
+++ PIQService/PIQService.Api/Controllers/UserScoresController.cs
@@ -14,6 +14,9 @@ namespace PIQService.Api.Controllers;
 [Route("user-scores")]
 public class UserScoresController(IScoreService scoreService) : ControllerBase
 {
+    /// <summary>
+    /// Получение средних результатов пользователя
+    /// </summary>
     [HttpGet("{userId}/criteria-mean")]
     [SwaggerResponseExample(StatusCodes.Status200OK, typeof(UserMeanScoreDtoExample))]
     [ProducesResponseType<UserMeanScoreDto>(StatusCodes.Status200OK)]
@@ -26,13 +29,28 @@ public class UserScoresController(IScoreService scoreService) : ControllerBase
         return result.ToActionResult(this);
     }
 
+    /// <summary>
+    /// Получение средних результатов по команде
+    /// </summary>
     [HttpGet("teams/{teamId}/criteria-mean")]
+    [SwaggerResponseExample(StatusCodes.Status200OK, typeof(EnumerableUserMeanScoreDtoExample))]
+    [ProducesResponseType<List<UserMeanScoreDto>>(StatusCodes.Status200OK)]
+    [ProducesResponseType<string>(StatusCodes.Status404NotFound)]
+    [ProducesResponseType<string>(StatusCodes.Status403Forbidden)]
+    [ProducesResponseType<string>(StatusCodes.Status401Unauthorized)]
     public async Task<ActionResult<List<UserMeanScoreDto>>> GetTeamMeanScores(Guid teamId, [FromQuery] Guid? byAssessment = null)
     {
         var result = await scoreService.GetTeamMeanScoresAsync(teamId, User.ReadContextUser(), byAssessment);
         return result.ToActionResult(this);
     }
 
+    /// <summary>
+    /// Получение средних результатов всех студентов, доступных пользователю
+    /// </summary>
+    /// <remarks>
+    /// onlyWhereTutor - необязательный параметр, доступный только админам.
+    /// Если true - возвращаются студенты, где текущий пользователь куратор, false - возвращаются все студенты со всех команд
+    /// </remarks>
     [HttpGet("forms/{formId}/criteria-mean")]
     [Authorize(Roles = RolesConstants.AdminTutor)]
     [SwaggerResponseExample(StatusCodes.Status200OK, typeof(EnumerableUserMeanScoreDtoExample))]
