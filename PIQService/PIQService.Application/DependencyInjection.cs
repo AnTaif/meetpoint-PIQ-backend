749e5e2740092da49b20b5475c00cc3fe53534c3
Mikhail Gavrilyak <gavrilyak.mikhail@gmail.com>
2025-05-30 22:44:40 +0500

    feat: add redis hybrid cache

--- PIQService/PIQService.Application/DependencyInjection.cs
+++ PIQService/PIQService.Application/DependencyInjection.cs
@@ -1,3 +1,4 @@
+using Microsoft.Extensions.Caching.Hybrid;
 using Microsoft.Extensions.DependencyInjection;
 
 namespace PIQService.Application;
@@ -7,5 +8,16 @@ public static class DependencyInjection
     public static void AddApplicationLayer(this IServiceCollection services)
     {
         services.AddPIQServiceApplication();
+
+        services.AddHybridCache(options =>
+        {
+            options.MaximumPayloadBytes = 1024 * 1024;
+            options.MaximumKeyLength = 1024;
+            options.DefaultEntryOptions = new HybridCacheEntryOptions
+            {
+                Expiration = TimeSpan.FromMinutes(5),
+                LocalCacheExpiration = TimeSpan.FromMinutes(5),
+            };
+        });
     }
 }
